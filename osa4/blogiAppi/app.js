9352ee20977ccce1eaee87f4acf6323b000dfe28
Jasse Merivirta <jassemerivirta@Jasse-MBP.home>
2022-12-15 14:00:02 +0200

    blogs ja blog works

--- osa4/blogiAppi/app.js
+++ osa4/blogiAppi/app.js
@@ -3,41 +3,20 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const express = require('express')
+const blogsRouter = require('./controllers/blogs')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  logger.info('app toimii')
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 module.exports = app
