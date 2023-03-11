39a236af25ad2cc1ebb2656f095228d6d14421fb
Jasse Merivirta <jayzstep@gmail.com>
2023-03-11 13:00:12 +0200

    added view and hide buttons for blogs

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,32 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => (setVisible(!visible))
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  
+  const showBlog = {
+    display: visible ? '' : 'none'
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+      </div>
+      <div style ={showBlog}>
+        {blog.author} <br />{blog.url} <br /> likes: {blog.likes}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
