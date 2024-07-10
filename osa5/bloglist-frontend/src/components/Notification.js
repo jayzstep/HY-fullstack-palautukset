ab3126e252a61fea5be7721ad7d99502236b2463
Jasse Merivirta <jayzstep@gmail.com>
2024-07-10 18:34:30 +0300

    repo alustettu 2024 kurssisuoritusta varten

--- osa5/bloglist-frontend/src/components/Notification.js
+++
@@ -1,9 +0,0 @@
-const notificationStyle = {
-    color: 'red',
-    background: 'lightgrey'
-}
-const Notification = ({ message }) => (
-    <h1 style={notificationStyle}>{message}</h1>
-)
-
-export default Notification
