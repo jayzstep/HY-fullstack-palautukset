706eac88a8ae0f9fea57ac5666a34bb2cfb425e0
Jasse Merivirta <jayzstep@gmail.com>
2024-07-16 10:10:16 +0300

    kurssitiedot 2.1-2.5

---
+++ osa2/kurssitiedot/src/Course.jsx
@@ -0,0 +1,53 @@
+const Header = ({ name }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+    </div>
+  );
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <div>
+      <p>
+        {name} {exercises}
+      </p>
+    </div>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((s, p) => {
+    return s + p.exercises;
+  }, 0);
+
+  return (
+    <div>
+      <p>
+        <b>total of {total} exercises</b>
+      </p>
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course
