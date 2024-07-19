a34755f88501dc0af0da7eaf412d37f1e7e164c0
Jasse Merivirta <jayzstep@gmail.com>
2024-07-19 14:17:53 +0300

    osa4 blogilista, db-yhteys toimii, config ja logger toimii

--- osa4/index.js
+++ osa4/index.js
@@ -1,3 +1,6 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -12,8 +15,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
@@ -36,7 +38,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
