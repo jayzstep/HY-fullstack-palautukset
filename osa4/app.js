416b8ee6c95e2e01198f53387ea7319086163b80
Jasse Merivirta <jayzstep@gmail.com>
2024-07-19 15:29:21 +0300

    omat middlewaret lisätty

--- osa4/app.js
+++ osa4/app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -23,6 +24,11 @@ app.use(cors())
 // app.use(express.static('dist'))
 app.use(express.json())
 
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
