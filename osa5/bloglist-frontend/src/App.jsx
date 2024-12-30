da29aebfd4b9b4668ba7eb779d70c60924c6e4de
Jasse <jasse.merivirta@helsinki.fi>
2024-12-30 16:47:31 +0200

    siirrä username/password tilojen hallinta loginformille

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -2,13 +2,10 @@ import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import LoginForm from "./components/Loginform";
 import blogService from "./services/blogs";
-import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [message, setMessage] = useState(null);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
@@ -27,14 +24,6 @@ const App = () => {
     }
   }, []);
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
   const flash = (aMessage) => {
     setMessage(aMessage);
     setTimeout(() => {
@@ -42,19 +31,6 @@ const App = () => {
     }, 5000);
   };
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
-    try {
-      const user = await loginService.login({ username, password });
-      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
-      blogService.setToken(user.token);
-      setUser(user);
-      setUsername("");
-      setPassword("");
-    } catch (exception) {
-      flash("wrong credentials");
-    }
-  };
   const handleCreate = async (event) => {
     event.preventDefault();
     try {
@@ -122,15 +98,7 @@ const App = () => {
     <div>
       <h1>BlogApp</h1>
       <Notification message={message} />
-      {!user && (
-        <LoginForm
-          handleLogin={handleLogin}
-          handleUsernameChange={handleUsernameChange}
-          handlePasswordChange={handlePasswordChange}
-          username={username}
-          password={password}
-        />
-      )}
+      {!user && <LoginForm flash={flash} setUser={setUser} />}
       {user && (
         <div>
           <p>{user.name} logged in</p>
--- osa5/bloglist-frontend/src/components/Loginform.jsx
+++ osa5/bloglist-frontend/src/components/Loginform.jsx
@@ -1,34 +1,50 @@
-const LoginForm = ({
-  handleLogin,
-  handleUsernameChange,
-  handlePasswordChange,
-  username,
-  password
-}) => (
-  <div>
-    <h2>Login</h2>
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={handleUsernameChange}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={handlePasswordChange}
-        />
-      </div>
-      <button type="submit">Login</button>
-    </form>
-  </div>
-);
+import loginService from "../services/login";
+import blogService from "../services/blogs";
+import { useState } from "react";
+
+const LoginForm = ({ flash, setUser }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
+      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      flash("wrong credentials");
+    }
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>);
+
+};
 
 export default LoginForm;
