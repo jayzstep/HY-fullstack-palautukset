55488f0221d61bf199b85bf5e565ebd6cfebd559
Jasse <jasse.merivirta@helsinki.fi>
2024-12-30 13:40:51 +0200

    lisää localstorage ja logout-näppäin

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -14,14 +14,24 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
       const user = await loginService.login({ username, password });
+      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
       blogService.setToken(user.token);
       setUser(user);
-      setUsername('')
-      setPassword('')
+      setUsername("");
+      setPassword("");
     } catch (exception) {
       setErrorMessage("wrong credentials");
       setTimeout(() => {
@@ -29,6 +39,11 @@ const App = () => {
       }, 5000);
     }
   };
+
+  const handleLogout = async (event) => {
+    window.localStorage.clear()
+    setUser(null)
+  }
 
   const loginForm = () => (
     <div>
@@ -73,6 +88,7 @@ const App = () => {
       {user && (
         <div>
           <p>{user.name} logged in</p>
+          <button onClick={handleLogout}>logout</button>
           <h2>blogs</h2>
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
