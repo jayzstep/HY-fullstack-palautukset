c81a51b2e198d459185911cee152536182aa33d4
Jasse <jasse.merivirta@helsinki.fi>
2025-01-05 16:20:43 +0200

    testaa blogin lisäyslomakkeen toimivuus

--- osa5/bloglist-frontend/src/components/NewBlogForm.test.jsx
+++ osa5/bloglist-frontend/src/components/NewBlogForm.test.jsx
@@ -4,11 +4,9 @@ import userEvent from '@testing-library/user-event'
 
 test('<NewBlogForm /> updates parent state', async () => {
   const user = userEvent.setup()
-  const mockSetBlogs = vi.fn()
-  const mockFlash = vi.fn()
-  const mockToggleVisibility = vi.fn()
+  const mockHandleCreate = vi.fn()
 
-  render(<NewBlogForm  flash={mockFlash} setBlogs={mockSetBlogs} toggleVisibility={mockToggleVisibility}  />)
+  render(<NewBlogForm  handleCreate={mockHandleCreate}  />)
 
   const titleInput = screen.getByPlaceholderText('blog title')
   const authorInput = screen.getByPlaceholderText('John Smith')
@@ -16,8 +14,20 @@ test('<NewBlogForm /> updates parent state', async () => {
   const sendButton = screen.getByText('Create')
 
   await user.type(titleInput, 'Teoksen nimi')
+  await user.type(authorInput, 'Tekijä')
+  await user.type(urlInput, 'www.osoite.fi')
   await user.click(sendButton)
 
-  expect(mockFlash.mock.calls).toHaveLength(1)
-  // expect(createNote.mock.calls[0][0].content).toBe('testing a form...')
+  const blog = {
+    title: 'Teoksen nimi',
+    author: 'Tekijä',
+    url: 'www.osoite.fi',
+    likes: 0
+  }
+
+  expect(mockHandleCreate.mock.calls).toHaveLength(1)
+  expect(mockHandleCreate.mock.calls[0][0].title).toBe(blog.title)
+  expect(mockHandleCreate.mock.calls[0][0].author).toBe(blog.author)
+  expect(mockHandleCreate.mock.calls[0][0].url).toBe(blog.url)
+  expect(mockHandleCreate.mock.calls[0][0].likes).toBe(0)
 })
