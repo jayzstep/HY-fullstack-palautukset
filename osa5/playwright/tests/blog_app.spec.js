fbe6b1fc46f7d39a3153a907d0939c1ab6fc1e84
Jasse <jasse.merivirta@helsinki.fi>
2025-01-08 22:19:46 +0200

    lisää testi: vain blogin luonut käyttäjä voi sen poistaa

--- osa5/playwright/tests/blog_app.spec.js
+++ osa5/playwright/tests/blog_app.spec.js
@@ -37,31 +37,43 @@ describe('Blog app', () => {
   describe('when logged in', () => {
     beforeEach(async ({ page }) => {
       await loginWith(page, 'merivja', 'salainen')
+      await createBlog(page, 'testiblogi', 'Jaakko Testi', 'www.testi.fi')
     })
 
     test('a new blog can be created', async ({ page }) => {
-      await createBlog(page, 'testiblogi', 'Jaakko Testi', 'www.testi.fi')
       await expect(page.getByTestId('blog-title')).toBeVisible()
     })
 
     test('a blog can be liked', async ({ page }) => {
-      await createBlog(page, 'testiblogi', 'Jaakko Testi', 'www.testi.fi')
       await page.getByRole('button', { name: 'show' }).click()
       await page.getByRole('button', { name: 'like' }).click()
       await expect(page.getByText('likes: 1')).toBeVisible()
     })
 
     test('a blog can be deleted', async ({ page }) => {
-      await createBlog(page, 'testiblogi', 'Jaakko Testi', 'www.testi.fi')
-
       page.on('dialog', async dialog => {
         await dialog.accept()
       })
-
       await page.getByRole('button', { name: 'show' }).click()
       await page.getByRole('button', { name: 'delete blog' }).click()
       await expect(page.getByTestId('blog-title')).not.toBeVisible()
 })
+
+    test('delete button visible only to creator of blog', async ({ page, request }) => {
+      await createBlog(page, 'testiblogi', 'Jaakko Testi', 'www.testi.fi')
+      await page.getByRole('button', { name: 'logout' }).click()
+      await request.post('http://localhost:3001/api/users', {
+        data: {
+          name: 'Rilke',
+          username: 'rilke',
+          password: 'tuulella'
+        }
+      })
+
+      await loginWith(page, 'rilke', 'tuulella')
+      await page.getByRole('button', { name: 'show' }).click()
+      await expect(page.getByRole('button', {name: 'delete blog'})).not.toBeVisible()
+    })
 
 })
 })
