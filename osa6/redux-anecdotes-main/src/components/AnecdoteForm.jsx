6d0bf67816867a8354fefa3907078f1cb5aaca08
Jasse <jasse.merivirta@helsinki.fi>
2025-01-12 21:57:45 +0200

    refaktoroi form ja list omiksi komponenteiksi

---
+++ osa6/redux-anecdotes-main/src/components/AnecdoteForm.jsx
@@ -0,0 +1,30 @@
+import { useSelector, useDispatch } from "react-redux";
+import { newAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm = () => {
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+
+  const addAnecdote = (event) => {
+
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    dispatch(newAnecdote(content));
+  };
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
