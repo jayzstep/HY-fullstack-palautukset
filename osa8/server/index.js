602aa280fc2a4e021bd38dd3402e4e01c00f2281
Jasse <jasse.merivirta@helsinki.fi>
2025-06-12 10:00:33 +0300

    8.16 lisää käyttäjienhallinta

--- osa8/server/index.js
+++ osa8/server/index.js
@@ -1,13 +1,13 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
-const { v1: uuid } = require('uuid')
-
+const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
 const Book = require('./models/book')
 const Author = require('./models/author')
+const User = require('./models/user')
 const { GraphQLError } = require('graphql')
 
+mongoose.set('strictQuery', false)
 require('dotenv').config()
 
 const MONGODB_URI = process.env.MONGODB_URI
@@ -123,11 +123,22 @@ id: ID!
 genres: [String!]!
 }
 
+type User {
+username: String!
+favoriteGenre: String!
+id: ID!
+}
+
+type Token {
+value: String!
+}
+
 type Query {
 bookCount: Int!
 authorCount: Int!
 allBooks(author: String, genre: String): [Book!]!
 allAuthors: [Author!]!
+me: User
 }
 
 type Mutation {
@@ -142,6 +153,16 @@ editAuthor(
 name: String!
 setBornTo: Int!
 ): Author
+
+createUser(
+username: String!
+favoriteGenre: String!
+): User
+
+login(
+username: String!
+password: String!
+): Token
 }
 `
 
@@ -160,7 +181,10 @@ const resolvers = {
       }
       return await Book.find(bookQuery).populate('author')
     },
-    allAuthors: async () => await Author.find({})
+    allAuthors: async () => await Author.find({}),
+    me: (root, args, context) => {
+      return context.currentUser
+    }
   },
   Author: {
     bookCount: async root => {
@@ -168,7 +192,15 @@ const resolvers = {
     }
   },
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, { currentUser }) => {
+      if (!currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT'
+          }
+        })
+      }
+
       let author = await Author.findOne({ name: args.author })
       if (!author) {
         author = new Author({ name: args.author })
@@ -197,7 +229,14 @@ const resolvers = {
         })
       }
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, { currentUser }) => {
+      if (!currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT'
+          }
+        })
+      }
       try {
         return await Author.findOneAndUpdate(
           { name: args.name },
@@ -215,6 +254,37 @@ const resolvers = {
           }
         })
       }
+    },
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre })
+
+      return user.save()
+        .catch(error => {
+          throw new GraphQLError('Creating the user failed', {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args.username,
+              error
+            }
+          })
+        })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== 'secret') {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT'
+          }
+        })
+      }
+      const userForToken = {
+        username: user.username,
+        id: user._id
+      }
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
     }
   }
 }
@@ -225,7 +295,15 @@ const server = new ApolloServer({
 })
 
 startStandaloneServer(server, {
-  listen: { port: 4000 }
+  listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.startsWith('Bearer ')) {
+      const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
